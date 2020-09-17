7792975e3769f23ec95537dd1060b3732618f6ca
VitorGrego <51011380+VitorGrego@users.noreply.github.com>
2020-09-17 14:58:31 -0300

    Add files via upload

---
+++ Urna-eletronica-js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+	{
+		titulo:'VEREADOR',
+		numeros:5,
+		candidatos:[
+			{
+				numero:'38111',
+				nome:'Fulano de tal',
+				partido:'ABC',
+				fotos:[
+					{url:'do-utilizador.png', legenda:'Vereador'}
+				]
+			},
+			{
+				numero:'77222',
+				nome:'Beltrano da Silva',
+				partido:'DEFG',
+				fotos:[
+					{url:'do-utilizador.png', legenda:'Vereador'}
+				]
+			},
+		]
+	},
+	{
+		titulo:'PREFEITO',
+		numeros:2,
+		candidatos:[
+			{
+				numero:'99',
+				nome:'Ciclano',
+				partido:'ABC',
+				vice:'Cic',
+				fotos:[
+					{url:'do-utilizador.png', legenda:'Prefeito'},
+					{url:'do-utilizador.png', legenda:'Vice-Prefeito', small:true}
+				]
+			},
+			{
+				numero:'84',
+				nome:'Zulano',
+				partido:'QWERTY',
+				vice:'Zul',
+				fotos:[
+					{url:'do-utilizador.png', legenda:'Prefeito'},
+					{url:'do-utilizador.png', legenda:'Vice-Prefeito', small:true}
+				]
+			},
+		]
+	}
+];
